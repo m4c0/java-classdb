bf5363bb4ac2173758736059d1515e74b0bf805b
Eduardo Costa <m4c0@users.noreply.github.com>
2026-07-03 16:22:14 +0100

    support to load classes of jars

--- main.c
+++ main.c
@@ -12,6 +12,7 @@ static int usage() {
   fprintf(stderr, "Usage:\n\n");
   fprintf(stderr, "    %s <command> <args...>\n\n", argv0);
   fprintf(stderr, "Where command can be:\n\n");
+  fprintf(stderr, "    add_jar - loads class from a jar into the DB\n\n");
   fprintf(stderr, "    reset - creates a new DB, destroying existing one\n\n");
   return 1;
 }
@@ -33,12 +34,13 @@ static char * slurp(const char * file) {
   return data;
 }
 
-static int sql_check(int rc, const char * msg) {
-  if (rc == SQLITE_OK) return 0;
+static int sql_check(int rc, int exp, const char * msg) {
+  if (rc == exp) return 0;
   fprintf(stderr, "%s: %s\n%s", msg, sqlite3_errstr(rc), sqlite3_errmsg(db));
   return 1;
 }
-#define _(x) if (sql_check((x), #x)) return 1;
+#define _chk(x, exp) if (sql_check((x), exp, #x)) return 1;
+#define _(x) _chk(x, SQLITE_OK)
 
 int run_reset(int argc, char ** argv) {
   if (argc != 0) return usage();
@@ -51,10 +53,50 @@ int run_reset(int argc, char ** argv) {
   return 0;
 }
 
+int run_add_jar(int argc, char ** argv) {
+  if (argc == 0) return usage();
+
+  char buf[1024];
+  snprintf(buf, 1024, "jar tf '%s'", *argv);
+  FILE * f = popen(buf, "r");
+  assert(f);
+
+  sqlite3_stmt * stmt;
+  _(sqlite3_prepare_v2(db,
+        "INSERT INTO class (jar, name, fqn) VALUES (?, ?, ?)", -1,
+        &stmt, NULL));
+
+  while (fgets(buf, 1024, f)) {
+    buf[strlen(buf) - 1] = 0; // Chop EOL
+
+    char * ext = strrchr(buf, '.');
+    if (!ext) continue;
+    if (strcmp(ext, ".class")) continue;
+
+    *ext = 0;
+    char * name = strrchr(buf, '/');
+    name = name ? name + 1 : buf;
+    if (0 == strcmp(name, "/package-info")) continue;
+
+    _(sqlite3_reset(stmt));
+    _(sqlite3_bind_text(stmt, 1, *argv, -1, NULL));
+    _(sqlite3_bind_text(stmt, 2, name,  -1, NULL));
+    _(sqlite3_bind_text(stmt, 3, buf,   -1, NULL));
+    _chk(sqlite3_step(stmt), SQLITE_DONE);
+  }
+
+  pclose(f);
+
+  sqlite3_finalize(stmt);
+
+  return 0;
+}
+
 int run(int argc, char ** argv) {
   if (argc == 0) return usage();
 
-  if (0 == strcmp(*argv, "reset")) return run_reset(--argc, ++argv);
+  if      (0 == strcmp(*argv, "reset"  )) return run_reset  (--argc, ++argv);
+  else if (0 == strcmp(*argv, "add_jar")) return run_add_jar(--argc, ++argv);
   else return usage();
 }
 
